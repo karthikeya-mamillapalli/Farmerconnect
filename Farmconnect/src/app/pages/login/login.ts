0098f4b7ae423831eaf13307368385b52d57d016
karthikeya <nithya.041591@gmail.com>
2026-05-05 01:44:53 +0530

    login tab switch activated

--- Farmconnect/src/app/pages/login/login.ts
+++ Farmconnect/src/app/pages/login/login.ts
@@ -1,28 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { UserLogin } from '../../core/models/classes/User.Model';
 import { UserService } from '../../core/services/user-service';
-import { Role } from '../../core/enums/Role.enum';  
+import { Role } from '../../core/enums/Role.enum';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-    userServ=inject(UserService)
+  activeTab: string = 'login';
+  showTab(tab: string) {
+    this.activeTab = tab;
+  }
+  userServ = inject(UserService);
 
   loginObj: UserLogin = {
     userName: '',
     password: '',
-    role:0
-  }
+    role: 0,
+  };
 
-  onLogin(){
-    const loginDat={};
-    this.loginObj.role=Role.Farmer
-    this.userServ.login(this.loginObj).subscribe({
-
-    })
+  onLogin() {
+    const loginDat = {};
+    this.loginObj.role = Role.Farmer;
+    this.userServ.login(this.loginObj).subscribe({});
   }
 }
